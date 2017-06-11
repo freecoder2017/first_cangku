b18d696d190cbea5b6d30505e8a964bf08f7b81f
liyuanjie3014 <377510010@qq.com>
2017-06-11 15:04:08 +0800

    first_rocord
    第一次记录工程20170611-15:04

---
+++ hahaha/Assets/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody yw = null;
+    // Use this for initialization
+    void Start()
+    {
+        yw = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        yw.AddForce(new Vector3(h, 0, v) * 10);
+    }
+}
